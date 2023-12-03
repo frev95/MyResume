cc286ad0b4c9e15484dff7d84a3cf29283c56e88
frev95 <gofrev@gmail.com>
2023-12-03 17:53:28 +0100

    Ajout des polices

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -17,7 +17,7 @@ const Navigation = ({ page }) => {  // la prop page applique à la page courante
                 <div className={page==2 ? "nav current" : "nav item"}>Compétences</div>
             </Link>
             <Link to="/portfolio">
-                <div className={page==3 ? "nav current" : "nav item"}>Porfolio</div>
+                <div className={page==3 ? "nav current" : "nav item"}>Portfolio</div>
             </Link>
             <Link to="/contact">
                 <div className={page==4 ? "nav current" : "nav item"}>Contact</div>
