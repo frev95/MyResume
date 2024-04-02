13ef628e192f760182c85f6c01306cdafb5a1e2e
frev95 <gofrev@gmail.com>
2024-04-02 17:16:24 +0200

    Dégradé de transparence

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,14 +5,18 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <footer>
-            <div>
-                <span className="coords">François Réveillon</span>
-                <span className="coords"><i className="fas fa-envelope"></i>{" "}adresse@domaine.fr</span>
-                <span className="coords"><i className="fas fa-phone"></i>{" "}01.23.45.67.89</span>
+        <>
+            <div className="transition">
             </div>
-            <div className="rights">©2023 Tous droits réservés</div>
-        </footer>
+            <footer>
+                <div>
+                    <span className="coords">François Réveillon</span>
+                    <span className="coords"><i className="fas fa-envelope"></i>{" "}adresse@domaine.fr</span>
+                    <span className="coords"><i className="fas fa-phone"></i>{" "}01.23.45.67.89</span>
+                </div>
+                <div className="rights">©2023 Tous droits réservés</div>
+            </footer>
+        </>
     );
 }
 
